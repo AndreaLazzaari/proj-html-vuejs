727df54a90e0364d30077dbd5a04a2a0f7236ea6
Andrea Lazzari <andrea.lazzari8@gmail.com>
2024-01-09 12:31:59 +0100

    create 4 card dentro componente SectionServices

--- proj-html-vuejs/src/components/SectionServices.vue
+++ proj-html-vuejs/src/components/SectionServices.vue
@@ -1,6 +1,49 @@
 <template lang="">
-    <div>
-        
+    <div class='container mt-5 p-1'>
+        <a href="#">Available services</a>
+        <div class='row'>
+            <div class='col-3'>
+                <div class="card mt-5" style="width: 18rem;">
+                    <div class="card-body blue">
+                        <h5 class="card-title fs-1 p-4">Answering phone calls</h5>
+                        <p class="card-text p-3 lh-base">Elementum posuere mauris, ac ultricies eu orci massa at id tindident.</p>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div class='col-3'>
+                <div class="card mt-5" style="width: 18rem;">
+                    <div class="card-body blue">
+                        <h5 class="card-title fs-1 p-4">Basic data entry</h5>
+                        <p class="card-text p-3 lh-base">Accumanas odio id nec mauris, eu, tortor at. Vitae tincidunt enim quis.</p>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div class='col-3'>
+                <div class="card mt-5" style="width: 18rem;">
+                    <div class="card-body blue">
+                        <h5 class="card-title fs-1 p-4">Organizing calendar</h5>
+                        <p class="card-text p-3 lh-base">Elementum blandit erat odio quam pellentesque aliquam faucibus.</p>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div class='col-3'>
+                <div class="card mt-5" style="width: 18rem;">
+                    <div class="card-body blue">
+                        <h5 class="card-title fs-1 p-4">Booking travels</h5>
+                        <p class="card-text p-3 lh-base">Est dignissim ipsum tellus, hendrerit ac aliquam eu vel blandit ac amet ut ut.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -10,6 +53,12 @@ export default {
 }
 </script>
 
-<style lang="">
+<style lang="scss">
+@use '../styles/general.scss' as *;
+@use '../styles/partials/variables' as *;
     
+
+    .blue{
+        color: $blue-electric;
+    }
 </style>
